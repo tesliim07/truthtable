8f3fe525c1e3f3e107aa4a8964cece93164959cb
tesliim07 <95727782+tesliim07@users.noreply.github.com>
2022-01-06 20:55:49 +0100

    4 input truth table

--- test.py
+++ test.py
@@ -1,12 +1,59 @@
 def getInputs():
-    amount_of_inputs=int(input("Enter the amount of inputs:"))
     variables=[]
-    while len(variables)<amount_of_inputs:
-        variable=str(input("Enter the variable:")).upper()[0]
-        while variable.isalpha()==False:
-            variable=str(input("Enter the variable:")).upper()[0]
+    inputAmount=input("Enter all the variables in the equation: ").upper()
+    for i in range(len(inputAmount)):
+        if inputAmount[i] in variables:
             continue
-        variables.append(variable)
-    return amount_of_inputs
+        else:
+            variables.append(inputAmount[i])
+    variables.sort()
+    return variables
 
-getInputs()
+def truthTable():
+    letters=getInputs()
+    if len(letters)==1:
+        string=''
+        for i in range(len(letters)):
+            string=string+letters[i]+'|'
+        print(string)
+        for a in range(0,2):
+            print(a)
+
+    elif len(letters)==2:
+        string=''
+        for i in range(len(letters)):
+            string=string+letters[i]+'|'
+        print(string)
+        for a in range(0,2):
+            for b in range(0,2):
+                print(a,b)
+
+    elif len(letters)==3:
+        string=''
+        for i in range(len(letters)):
+            string=string+letters[i]+'|'
+        print(string)
+        for a in range(0,2):
+            for b in range(0,2):
+                for c in range(0,2):
+                    print(a,b,c)
+
+    elif len(letters)==4:
+        string=''
+        for i in range(len(letters)):
+            string=string+letters[i]+'|'
+        print(string)
+        for a in range(0,2):
+            for b in range(0,2):
+                for c in range(0,2):
+                    for d in range(0,2):
+                        print(a,b,c,d)       
+
+    
+truthTable()
+
+    
+
+
+
+
